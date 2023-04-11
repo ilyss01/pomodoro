89f761132d737536443b9c8952d18bbe304f0598
Ilya Romanuk <ilya.romanuck2014@yandex.ru>
2023-04-11 16:53:10 +0300

    Moved from array of args to OranizedArgs struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::env;
-use std::io;
 use std::process::exit;
 use std::thread::sleep;
 use std::time::Duration;
 
 // TODO: play sound on ending the session?
+// TODO: proper error handling == remove unwrap's
 
-fn print_error_message() {
-    println!("Matched undocumented case, exiting program")
+struct OrganizedArgs {
+    work_time: u32,
+    short_break: u32,
+    long_break: u32,
+    long_break_interval: u32,
 }
 
 fn sleep_handle(time: Duration, str: String) {
@@ -16,48 +19,48 @@ fn sleep_handle(time: Duration, str: String) {
     if str == "Break" {
         what_to_do = "start working".to_string();
     } else {
-        what_to_do = "chill".to_string();
+        what_to_do = "relax".to_string();
     }
     println!("{} time has ended, press enter to {}", str, what_to_do);
-    // TODO: change stdin to something more sane
-    io::stdin();
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // args are organized to [work_time, short_break, long_break, long_break_interval]
-    let mut organized_args: [u64; 4] = [0; 4];
-    let mut cycle: u64 = 0;
+    let mut organized_args = OrganizedArgs {
+        work_time: 25,
+        short_break: 5,
+        long_break: 15,
+        long_break_interval: 4,
+    };
+    let mut cycle: u32 = 0;
     let work_time: Duration;
     let short_break_time: Duration;
     let long_break_time: Duration;
-    let long_break_interval: u64;
+    let long_break_interval: u32;
 
     // TODO: find another way to parse arguments, put them in another function
+    // TODO: add --help
+    // transition from seconds to minutes will happen not there
     match args.len() {
         // default case when the program is executed without any args e.g.
-        // pomodoro
-        1 => {
-            organized_args = [25 * 60, 5 * 60, 15 * 60, 4];
-        }
-        // pomodoro 25 5 15 4
+        // e.g. pomodoro 25 5 15 4
         5 => {
-            for i in 0..args.len() {
-                organized_args[i] = 60 * args[i].parse::<u64>().unwrap();
-            }
-            organized_args[3] = organized_args[3] / 60;
+            organized_args.work_time = args[1].parse().unwrap();
+            organized_args.short_break = args[2].parse().unwrap();
+            organized_args.long_break = args[3].parse().unwrap();
+            organized_args.long_break_interval = args[4].parse().unwrap();
         }
-        // pomodoro --work 25 --short 5 --long 20 --cycles 4
+        // e.g. pomodoro --work 25 --short 5 --long 20 --cycles 4
         9 => {
             for i in 0..args.len() {
                 if args[i] == "--work" {
-                    organized_args[0] = args[i + 1].parse::<u64>().unwrap();
+                    organized_args.work_time = args[i + 1].parse().unwrap();
                 } else if args[i] == "--short" {
-                    organized_args[1] = args[i + 1].parse::<u64>().unwrap();
+                    organized_args.short_break = args[i + 1].parse().unwrap();
                 } else if args[i] == "--long" {
-                    organized_args[2] = args[i + 1].parse::<u64>().unwrap();
+                    organized_args.long_break = args[i + 1].parse().unwrap();
                 } else if args[i] == "--cycles" {
-                    organized_args[3] = args[i + 1].parse::<u64>().unwrap();
+                    organized_args.long_break_interval = args[i + 1].parse().unwrap();
                 } else {
                     println!("Can't handle arguments, exiting the program");
                     exit(-1);
@@ -65,16 +68,17 @@ fn main() {
             }
         }
         _ => {
-            print_error_message();
             exit(-1);
         }
     }
 
-    work_time = Duration::new(60 * organized_args[0], 0);
-    short_break_time = Duration::new(60 * organized_args[1], 0);
-    long_break_time = Duration::new(60 * organized_args[2], 0);
-    long_break_interval = organized_args[3];
+    // seconds -> minutes -> duration struct
+    work_time = Duration::new(60 * organized_args.work_time as u64, 0);
+    short_break_time = Duration::new(60 * organized_args.short_break as u64, 0);
+    long_break_time = Duration::new(60 * organized_args.long_break as u64, 0);
+    long_break_interval = organized_args.long_break_interval;
 
+    // main loop
     loop {
         sleep_handle(work_time, "Work".to_string());
 
