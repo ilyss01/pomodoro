cafbf18fcd9a29a4c6fdf11b5df12d9059c5b907
Ilya Romanuk <ilya.romanuck2014@yandex.ru>
2023-04-12 13:20:56 +0300

    Moved argument parsing to seperate function

--- src/main.rs
+++ src/main.rs
@@ -24,53 +24,70 @@ fn sleep_handle(time: Duration, str: String) {
     println!("{} time has ended, press enter to {}", str, what_to_do);
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut organized_args = OrganizedArgs {
-        work_time: 25,
-        short_break: 5,
-        long_break: 15,
-        long_break_interval: 4,
-    };
-    let mut cycle: u32 = 0;
-    let work_time: Duration;
-    let short_break_time: Duration;
-    let long_break_time: Duration;
-    let long_break_interval: u32;
-
+fn parse_args(args: Vec<String>) -> OrganizedArgs {
     // TODO: find another way to parse arguments, put them in another function
     // TODO: add --help
     // transition from seconds to minutes will happen not there
     match args.len() {
+        2 => {
+            if args[1] == "--help" || args[1] == "-h" {
+                println!("Help page, hi");
+                exit(0);
+            } else if args[1] == "--version" || args[1] == "-v" {
+                println!("{}", env!("CARGO_PKG_VERSION"));
+                exit(0);
+            } else {
+                println!("Unknown args, exiting");
+                exit(-1);
+            }
+        }
         // default case when the program is executed without any args e.g.
         // e.g. pomodoro 25 5 15 4
-        5 => {
-            organized_args.work_time = args[1].parse().unwrap();
-            organized_args.short_break = args[2].parse().unwrap();
-            organized_args.long_break = args[3].parse().unwrap();
-            organized_args.long_break_interval = args[4].parse().unwrap();
-        }
+        5 => OrganizedArgs {
+            work_time: args[1].parse().unwrap(),
+            short_break: args[2].parse().unwrap(),
+            long_break: args[3].parse().unwrap(),
+            long_break_interval: args[4].parse().unwrap(),
+        },
         // e.g. pomodoro --work 25 --short 5 --long 20 --cycles 4
         9 => {
-            for i in 0..args.len() {
+            let mut temp_struct = OrganizedArgs {
+                work_time: 25,
+                short_break: 5,
+                long_break: 20,
+                long_break_interval: 4,
+            };
+            for i in 1..args.len() {
                 if args[i] == "--work" {
-                    organized_args.work_time = args[i + 1].parse().unwrap();
+                    temp_struct.work_time = args[i + 1].parse().unwrap();
                 } else if args[i] == "--short" {
-                    organized_args.short_break = args[i + 1].parse().unwrap();
+                    temp_struct.short_break = args[i + 1].parse().unwrap();
                 } else if args[i] == "--long" {
-                    organized_args.long_break = args[i + 1].parse().unwrap();
+                    temp_struct.long_break = args[i + 1].parse().unwrap();
                 } else if args[i] == "--cycles" {
-                    organized_args.long_break_interval = args[i + 1].parse().unwrap();
+                    temp_struct.long_break_interval = args[i + 1].parse().unwrap();
                 } else {
                     println!("Can't handle arguments, exiting the program");
                     exit(-1);
                 }
             }
+            temp_struct
         }
         _ => {
+            println!("Can't handle arguments, exiting program");
             exit(-1);
         }
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let organized_args = parse_args(args);
+    let mut cycle: u32 = 0;
+    let work_time: Duration;
+    let short_break_time: Duration;
+    let long_break_time: Duration;
+    let long_break_interval: u32;
 
     // seconds -> minutes -> duration struct
     work_time = Duration::new(60 * organized_args.work_time as u64, 0);
